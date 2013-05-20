02c95b13a9b76bcaadb1b2c23ccba84c9804b3a9
caplio <github@snugplace.net>
2013-05-20 12:38:01 +0900

    commit:dlxj settings
    Change-Id: I7679098ef0b973450102904d1cd57f3d79b138d5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -95,3 +95,4 @@ add_lunch_combo pac_maserati-userdebug
 add_lunch_combo pac_umts_spyder-userdebug
 add_lunch_combo pac_solana-userdebug
 add_lunch_combo pac_vanquishgsm-userdebug
+add_lunch_combo pac_dlxj-userdebug
